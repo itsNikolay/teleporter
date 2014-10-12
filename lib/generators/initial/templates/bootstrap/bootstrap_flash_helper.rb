eccfd62a38aa273892db7892666528bdd7f892ea
Ponomarev Nikolay <itsnikolay@gmail.com>
2014-10-13 03:57:21 +0400

    hash

--- lib/generators/initial/templates/bootstrap/bootstrap_flash_helper.rb
+++ lib/generators/initial/templates/bootstrap/bootstrap_flash_helper.rb
@@ -16,8 +16,8 @@ module BootstrapFlashHelper
 
       Array(message).each do |msg|
         text = content_tag(:div,
-                           content_tag(:button, raw("&times;"), :class => "close", "data-dismiss" => "alert") +
-                           msg, :class => "alert fade in alert-#{type} #{options[:class]}")
+                           content_tag(:button, raw("&times;"), class: "close", "data-dismiss" => "alert") +
+                           msg, class: "alert fade in alert-#{type} #{options[:class]}")
         flash_messages << text if msg
       end
     end
