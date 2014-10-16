b192b5afa16854e82f5c6396d2474ccfe9407780
Ponomarev Nikolay <itsnikolay@gmail.com>
2014-10-17 01:43:08 +0400

    impove capistrano

--- lib/generators/initial/capistrano.rb
+++ lib/generators/initial/capistrano.rb
@@ -35,9 +35,21 @@ module Initial
                        "require 'capistrano/puma/monit'\n",
                        after: "require 'capistrano/sidekiq'\n"
 
-      copy_file 'capistrano/deploy/deploy.rb', 'config/deploy.rb'
-      copy_file 'capistrano/deploy/production.rb', 'config/deploy/production.rb'
+      @app_name = ask 'App name:'
+      @git_repo_url = ask 'Git repo (git@github.com:itsNikolay/puma-testing.git):'
+      @deploy_user = ask 'Deploy username (deploy):'
+      @ruby_version = ask 'Ruby version (2.1.2)'
+      @production_server_address = ask 'Production server (192.168.33.10):'
+      @add_staging = ask 'Add staging (y/n):'
 
+      template 'capistrano/deploy/deploy.rb', 'config/deploy.rb'
+      template 'capistrano/deploy/production.rb', 'config/deploy/production.rb'
+      if @add_staging =~ /y/
+        @staging_server_address = ask 'Staging server (192.168.33.10):'
+        template 'capistrano/deploy/staging.rb', 'config/deploy/staging.rb'
+      end
+
+      p 'to setup server run: $ cap staging deploy:setup_config'
     end
   end
 end
--- lib/teleporter/version.rb
+++ lib/teleporter/version.rb
@@ -1,3 +1,3 @@
 module Teleporter
-  VERSION = "1.0.0"
+  VERSION = "1.0.1"
 end
